7af3cc63115fec693353e2c7407fc02d16ec5085
IsacAnd <grimjaeguer@gmail.com>
2025-06-19 09:41:00 -0300

    fix: switching GET to POST on login route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -24,7 +24,7 @@ router.post("/register", async (req, res) => {
 });
 
 // Login
-router.get("/login", async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
